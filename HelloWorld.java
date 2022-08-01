2fdb07ab790b1366fedcaa66e8a7ba299cd579ca
Avi207 <60266462+Avi207@users.noreply.github.com>
2022-08-01 14:16:10 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
 	public static void main(String[] args){
-		System.out.println("Hello World");
+		System.out.println("You Have Successfully Integrated Git with Jenkins");
+		System.out.println("You can start building & testing you code from github");
 	}
 }
